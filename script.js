551404f2ea2512bd22a4de39c52390fac6be66cb
Sricharan N <90692839+Sricharan7113@users.noreply.github.com>
2023-05-25 19:09:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+  function SwapImage(isActive) {
+    var BehaviorImage = document.getElementById("BehaviorImg");
+    if (isActive) {
+        BehaviorImage.src="CloseYourEyes.png";
+    } else {
+        BehaviorImage.src="Monkey.png";
+    }
+  }
+
